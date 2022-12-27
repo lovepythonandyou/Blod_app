1df6506d40b312a9e0445b8269bcbcb3cfd46546
egor <eremkinegor58@gmail.com>
2022-12-27 17:52:22 +0300

    add forms, ModelForm

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
+
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
+from .forms import TagForm
 
 
 def posts_list(request):
@@ -17,13 +19,29 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={
+            'form': form
+        })
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(request, 'blog/tag_create.html', context={
+            'form': bound_form
+        })
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={
         'tags': tags
     })
-
-
-class TagDetail(ObjectDetailMixin, View):
-    model = Tag
-    template = 'blog/tag_detail.html'
